5deb660f9000a0aedc870cbc6d5fada079398eea
zhaowei <zhaowei123>
2020-12-29 00:24:17 +0800

    添加第三周星期一练习，反转二叉树

---
+++ Week_03/leetCode226/Solution1.java
@@ -0,0 +1,15 @@
+class Solution1 {
+    public TreeNode invertTree(TreeNode root) {
+
+        if(root == null){
+            return null;
+        }
+
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
+        return root;
+
+    }
+}
