d593f057e582a6cfa735192d51ff14dc9a78c690
zhaowei <zhaowei123>
2021-01-02 15:52:10 +0800

    添加第三周星期六练习，组合。

---
+++ Week_03/leetCode77/Solution.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> res = new ArrayList<>();
+        if (k <= 0 || n < k) {
+            return res;
+        }
+        Deque<Integer> path = new LinkedList<>();
+        dfs(n, k, 1, path, res);
+        return res;
+    }
+
+    private void dfs(int n, int k, int begin, Deque<Integer> path, List<List<Integer>> res) {
+        if (path.size() == k) {
+            res.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = begin; i <= n; i++) {
+            path.addLast(i);
+            dfs(n, k, i + 1, path, res);
+            path.removeLast();
+        }
+    }
+}
